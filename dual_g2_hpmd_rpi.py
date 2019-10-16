43c750e887eaca58538adcc11453679d63a755da
Kevin (Pololu) <dev-kevin@pololu.com>
2019-10-15 17:27:32 -0700

    changed interface library from WiringPi to pigpio; added getFault functions

--- dual_g2_hpmd_rpi.py
+++ dual_g2_hpmd_rpi.py
@@ -1,46 +1,37 @@
-import wiringpi
+import pigpio
 
-# Motor speeds for this library are specified as numbers
-# between -MAX_SPEED and MAX_SPEED, inclusive.
-_max_speed = 480  # 19.2 MHz / 2 / 480 = 20 kHz
+_pi = pigpio.pi()
+if not _pi.connected:
+    raise IOError("Can't connect to pigpio")
+
+# Motor speeds for this library are specified as numbers between -MAX_SPEED and
+# MAX_SPEED, inclusive.
+# This has a value of 480 for historical reasons/to maintain compatibility with
+# older libraries for other Pololu boards (which used WiringPi to set up the
+# hardware PWM directly).
+_max_speed = 480
 MAX_SPEED = _max_speed
 
-io_initialized = False
-def io_init():
-  global io_initialized
-  if io_initialized:
-    return
-
-  wiringpi.wiringPiSetupGpio()
-  wiringpi.pinMode(12, wiringpi.GPIO.PWM_OUTPUT)
-  wiringpi.pinMode(13, wiringpi.GPIO.PWM_OUTPUT)
-
-  wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
-  wiringpi.pwmSetRange(MAX_SPEED)
-  wiringpi.pwmSetClock(2)
-
-  wiringpi.pinMode(22, wiringpi.GPIO.OUTPUT)
-  wiringpi.pinMode(23, wiringpi.GPIO.OUTPUT)
-  wiringpi.pinMode(24, wiringpi.GPIO.OUTPUT)
-  wiringpi.pinMode(25, wiringpi.GPIO.OUTPUT)
-
-  io_initialized = True
+_pin_M1FLT = 5
+_pin_M2FLT = 6
+_pin_M1PWM = 12
+_pin_M2PWM = 13
+_pin_M1EN = 22
+_pin_M2EN = 23
+_pin_M1DIR = 24
+_pin_M2DIR = 25
 
 class Motor(object):
     MAX_SPEED = _max_speed
 
-    def __init__(self, pwm_pin, dir_pin, en_pin):
+    def __init__(self, pwm_pin, dir_pin, en_pin, flt_pin):
         self.pwm_pin = pwm_pin
         self.dir_pin = dir_pin
         self.en_pin = en_pin
+        self.flt_pin = flt_pin
 
-    def enable(self):
-        io_init()
-        wiringpi.digitalWrite(self.en_pin, 1)
-
-    def disable(self):
-        io_init()
-        wiringpi.digitalWrite(self.en_pin, 0)
+        _pi.set_pull_up_down(flt_pin, pigpio.PUD_UP) # make sure FLT is pulled up
+        _pi.write(en_pin, 1) # enable driver by default
 
     def setSpeed(self, speed):
         if speed < 0:
@@ -52,16 +43,29 @@ class Motor(object):
         if speed > MAX_SPEED:
             speed = MAX_SPEED
 
-        io_init()
-        wiringpi.digitalWrite(self.dir_pin, dir_value)
-        wiringpi.pwmWrite(self.pwm_pin, speed)
+        _pi.write(self.dir_pin, dir_value)
+        _pi.hardware_PWM(self.pwm_pin, 20000, int(speed * 6250 / 3));
+          # 20 kHz PWM, duty cycle in range 0-1000000 as expected by pigpio
+
+    def enable(self):
+        _pi.write(self.en_pin, 1)
+
+    def disable(self):
+        _pi.write(self.en_pin, 0)
+
+    def getFault(self):
+        return not _pi.read(self.flt_pin)
 
 class Motors(object):
     MAX_SPEED = _max_speed
 
     def __init__(self):
-        self.motor1 = Motor(12, 24, 22)
-        self.motor2 = Motor(13, 25, 23)
+        self.motor1 = Motor(_pin_M1PWM, _pin_M1DIR, _pin_M1EN, _pin_M1FLT)
+        self.motor2 = Motor(_pin_M2PWM, _pin_M2DIR, _pin_M2EN, _pin_M2FLT)
+
+    def setSpeeds(self, m1_speed, m2_speed):
+        self.motor1.setSpeed(m1_speed)
+        self.motor2.setSpeed(m2_speed)
 
     def enable(self):
         self.motor1.enable()
@@ -71,8 +75,15 @@ class Motors(object):
         self.motor1.disable()
         self.motor2.disable()
 
-    def setSpeeds(self, m1_speed, m2_speed):
-        self.motor1.setSpeed(m1_speed)
-        self.motor2.setSpeed(m2_speed)
+    def getFaults(self):
+        return self.motor1.getFault() or self.motor2.getFault()
+
+    def forceStop(self):
+        # reinitialize the pigpio interface in case we interrupted another command
+        # (so this method works reliably when called from an exception handler)
+        global _pi
+        _pi.stop()
+        _pi = pigpio.pi()
+        self.setSpeeds(0, 0)
 
 motors = Motors()
--- example.py
+++ example.py
@@ -2,6 +2,17 @@ from __future__ import print_function
 import time
 from dual_g2_hpmd_rpi import motors, MAX_SPEED
 
+# Define a custom exception to raise if a fault is detected.
+class DriverFault(Exception):
+    def __init__(self, driver_num):
+        self.driver_num = driver_num
+
+def raiseIfFault():
+    if motors.motor1.getFault():
+        raise DriverFault(1)
+    if motors.motor2.getFault():
+        raise DriverFault(2)
+
 # Set up sequences of motor speeds.
 test_forward_speeds = list(range(0, MAX_SPEED, 1)) + \
   [MAX_SPEED] * 200 + list(range(MAX_SPEED, 0, -1)) + [0]  
@@ -10,31 +21,41 @@ test_reverse_speeds = list(range(0, -MAX_SPEED, -1)) + \
   [-MAX_SPEED] * 200 + list(range(-MAX_SPEED, 0, 1)) + [0]  
 
 try:
-    motors.enable()
     motors.setSpeeds(0, 0)
 
     print("Motor 1 forward")
     for s in test_forward_speeds:
         motors.motor1.setSpeed(s)
-        time.sleep(0.005)
+        raiseIfFault()
+        time.sleep(0.002)
 
     print("Motor 1 reverse")
     for s in test_reverse_speeds:
         motors.motor1.setSpeed(s)
-        time.sleep(0.005)
+        raiseIfFault()
+        time.sleep(0.002)
+
+    # Disable the drivers for half a second.
+    motors.disable()
+    time.sleep(0.5)
+    motors.enable()
 
     print("Motor 2 forward")
     for s in test_forward_speeds:
         motors.motor2.setSpeed(s)
-        time.sleep(0.005)
+        raiseIfFault()
+        time.sleep(0.002)
 
     print("Motor 2 reverse")
     for s in test_reverse_speeds:
         motors.motor2.setSpeed(s)
-        time.sleep(0.005)
+        raiseIfFault()
+        time.sleep(0.002)
+
+except DriverFault as e:
+    print("Driver %s fault!" % e.driver_num)
 
 finally:
   # Stop the motors, even if there is an exception
   # or the user presses Ctrl+C to kill the process.
-  motors.setSpeeds(0, 0)
-  motors.disable()
+    motors.forceStop()
